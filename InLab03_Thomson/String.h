be272197be3ea285258e3383ff4124107a88c48b
Stephen <shimmergloom6@yahoo.com>
2020-04-25 18:56:10 -0700

    Header Ready

--- InLab03_Thomson/String.h
+++ InLab03_Thomson/String.h
@@ -21,13 +21,50 @@
 *	
 * Methods:
 *	GetLength
-*	GetStr
-*	Upper
-*	Lower
+*	GetString
 *	
 ************************************************************************************************************************/
+#include <iostream>
+using std::ostream;
+using std::istream;
 
 class String
 {
+	public:
+		String();
+		String(char ch);
+		String(const char * String);
+		String(const String& copy);
+		String(String&& copy);
+		~String();
+		String& operator=(const String& rhs);
+		String& operator=(String&& rhs);
+		bool operator ==(const String& Object) const;
+		bool operator ==(const char * String) const;
+		bool operator !=(const char* String) const;
+		bool operator !=(const String& Object) const;
+		bool operator <(const char* String) const;
+		bool operator <(const String& Object) const;
+		bool operator <=(const char* String) const;
+		bool operator <=(const String& Object) const;
+		bool operator >(const char* String) const;
+		bool operator >(const String& Object) const;
+		bool operator >=(const String& Object) const;
+		bool operator >=(const char* String) const;
+		friend ostream& operator << (ostream& out, const String& rhs);
+		friend istream& operator >> (istream& in, const String& rhs);
+		String operator +(const String& rhs);
+		String operator +=(const String& rhs);
+		String& operator ++();
+		String& operator ++(int);
+		String& operator --();
+		String& operator --(int);
+		char operator [](int i);
+		operator char* () const;
+		int GetLength();
+		const char * GetString() const;
 
+	private:
+		char * m_string;
+		int m_length;
 };
