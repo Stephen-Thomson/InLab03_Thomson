5d12aafde7bee2254982b09f18f176b25a91a33b
Stephen <shimmergloom6@yahoo.com>
2020-04-25 17:31:44 -0700

    Add project files.

---
+++ InLab03_Thomson/InLab03.cpp
@@ -0,0 +1,33 @@
+/**********************************************************************************************************************
+*
+* Author:		Stephen Thomson
+* Filename:		InLab03.cpp
+* Date Created:	4/26/2020
+*
+* Assignment InLab 03: Create and up to date String Class
+*
+* Overview:
+*	This program will test the functionality of the String class.
+*
+* Input:
+*	Noen
+*
+* Output:
+*	Strings and Messages
+*
+**********************************************************************************************************************/
+#define CRTBDG_MAP_ALLOC
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "String.h"
+#include <crtbdg.h>
+
+
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+}
---
+++ InLab03_Thomson/String.h
@@ -0,0 +1,33 @@
+#pragma once
+/***********************************************************************************************************************
+*
+* Class: String
+*
+* Purpose: Assigns a string and allows manipulation of the string.
+*
+* Manager Functions:
+*	Default Constructor
+*	String(char) Constructor
+*	Const char * argument Constructor
+*	Copy Constructor
+*	Move Constructor
+*	Destructor
+*	Overloaded Copy Assignment
+*	Overloaded Move Assignment
+*	Comparison Operators
+*	Overloaded << Operator
+*	Overloaded Operators
+*	Operator Conversion
+*	
+* Methods:
+*	GetLength
+*	GetStr
+*	Upper
+*	Lower
+*	
+************************************************************************************************************************/
+
+class String
+{
+
+};
